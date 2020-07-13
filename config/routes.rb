116328a43e22ebb77654ca9ed0c88d3cccc69814
Elijah wines <elijahwines84@yahoo.com>
2020-07-13 20:38:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get "/login", to: 'sessions#new'
-  post "/login", to: 'sessions#create'
-  get "/signup", to: 'users#new'
-  post "/signup", to: 'users#create'
-  get "/home", to: 'users#home'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
