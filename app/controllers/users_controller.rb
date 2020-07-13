2acbca0a1e7b5c5a9a65b09a79fe0675f931074c
Elijah wines <elijahwines84@yahoo.com>
2020-07-13 20:30:32 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
+    def new
+    end
 
-  def new
-  end
+    def create
+        @user = User.create(user_params)
+        return redirect_to "/signup" unless @user.save
+        session[:user_id] = @user.id
+    end
 
-  def create
-    User.create(user_params)
-  end
+    def home
+        user = User.find(session[:user_id])
+        puts "Hi, #{user.name}."
+    end
 
-  def home
-    user = User.find(session[:user_id])
-    puts "Hi, #{{user.name}}"
-  end
+    private
 
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
-  end
-
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 end
