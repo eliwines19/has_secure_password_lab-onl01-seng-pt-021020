cc9935f3969537c1ebe2b65f52118a33169c82bb
Elijah wines <elijahwines84@yahoo.com>
2020-07-13 20:21:22 +0000

    Automatically backed up by Learn

--- db/migrate/20200713201457_create_users.rb
+++ db/migrate/20200713201457_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string :username
+      t.password :password
+      t.password_confirmation :password_confirmation
 
       t.timestamps
     end
